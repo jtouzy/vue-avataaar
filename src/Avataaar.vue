53c2b15b05b6103168250bfa52c729757a8ccbf0
Nitwel <nitwel@arcor.de>
2020-03-31 01:42:41 +0200

    added random prop

--- src/Avataaar.vue
+++ src/Avataaar.vue
@@ -19,14 +19,25 @@ export default {
     hairColor: String,
     mouthType: String,
     skinColor: String,
-    topType: String
+    topType: String,
+    random: {
+      type: Boolean,
+      default: false
+    }
   },
   computed: {
     queryString() {
       let queryString =
         Object.keys(AvataaarMetadata)
           .filter(prop => this[prop])
-          .map(prop => `${prop}=${this[prop]}`)
+          .map(prop => {
+            if(this.random) {
+              let rand = Math.floor(Math.random() * AvataaarMetadata[prop].properties.length)
+              return `${prop}=${AvataaarMetadata[prop].properties[rand]}`
+            } else {
+              return `${prop}=${this[prop]}`
+            }
+          })
           .join('&')
       if (queryString.length) {
         queryString = `?${queryString}`
